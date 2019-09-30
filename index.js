0af442066c553c1e624382f290168fc093f7d9a1
Didier Zuñiga <didier35@hotmail.com>
2019-09-29 22:32:17 -0500

    [First commit]

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+
+//Body parser middleware
+app.use(express.json())
+
+moviesApi(app)
+
+app.use(logErrors)
+app.use(errorHandler)
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
---
+++ utils/mocks/movies.js
@@ -0,0 +1,46 @@
+const moviesMock = [
+  { 
+    "id":"9560a92b-a59c-4245-9d66-69d9314dc88a",
+    "title":"Diner",
+    "year":1997,
+    "cover":"http://dummyimage.com/171x194.jpg/dddddd/000000",
+    "description":"Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    "duration":2027,
+    "contentRating":"R",
+    "source":"http://reuters.com/odio/elementum/eu/interdum/eu.jpg",
+    "tags":[ 
+      "Drama"
+    ]
+  },
+  { 
+    "id":"aeb9a83d-f12d-4709-862a-39a3595707a8",
+    "title":"Swing Shift",
+    "year":2008,
+    "cover":"http://dummyimage.com/217x206.bmp/dddddd/000000",
+    "description":"In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    "duration":1962,
+    "contentRating":"R",
+    "source":"https://toplist.cz/eros.html",
+    "tags":[ 
+      "Action|Drama|Thriller",
+      "Crime|Drama|Fantasy|Mystery|Thriller"
+    ]
+  },
+  { 
+    "id":"43ad1a07-dbf6-4ce0-84aa-f233d5a7f6ff",
+    "title":"Masseurs and a Woman, The (Anma to onna)",
+    "year":1994,
+    "cover":"http://dummyimage.com/202x212.jpg/dddddd/000000",
+    "description":"Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    "duration":2055,
+    "contentRating":"PG-13",
+    "source":"https://about.me/nulla/justo/aliquam/quis/turpis.png",
+    "tags":[ 
+      "Action|Comedy|Crime|Drama|Thriller"
+    ]
+  }
+]
+
+module.exports = {
+  moviesMock
+}
